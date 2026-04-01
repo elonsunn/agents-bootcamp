73ed08fd063823f1f14a32521fcc4e3952ecb147
elon.sun <elon@Elons-Mac-mini.local>
2026-04-01 22:29:17 +0800

    summary

--- chatppt/src/utils.py
+++
@@ -1,8 +0,0 @@
-from pptx import Presentation
-
-def remove_all_slides(prs: Presentation):
-    xml_slides = prs.slides._sldIdLst
-    slides = list(xml_slides)
-    for slide in slides:
-        xml_slides.remove(slide)
-    print("所有默认幻灯片已被移除。")
